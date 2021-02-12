8877c4a3599e2e2355c7f81c47d910e34910ac9d
Adam Borbas <adam.borbas@bitrise.io>
2021-02-12 11:54:05 +0100

    Initial Commit

---
+++ macsample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macsample
+//
+//  Created by Adam Borbas on 2021. 02. 12..
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macsample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macsample
+//
+//  Created by Adam Borbas on 2021. 02. 12..
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
